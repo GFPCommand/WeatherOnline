f97a75c41e69a80dd55c20a2808a019f8f175f68
GFPCommand <49618224+GFPCommand@users.noreply.github.com>
2022-11-06 12:11:59 +0300

    Добавьте файлы проекта.

---
+++ Weather/Form2.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Weather
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
